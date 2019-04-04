36db361e2b4004b9390839d2ea29656e60ffa9ec
Joe Luchini <joeluchini@Joes-MacBook-Pro.local>
2019-04-04 17:24:35 -0600

    add subtract functions

---
+++ js/app2.js
@@ -0,0 +1,94 @@
+//---- Global Variables
+//first number
+var firstNum = 0;
+var firstNumResult = '';
+var isFirstNum = true;
+//operator
+var op;
+var isOp = true;
+var isAdd = false;
+var isMinus = false;
+//Second Number
+var secondNum = 0;
+var secondNumResult = '';
+var isSecondNum = true;
+//total 
+var total = 0;
+var isTotal = true;
+
+//------ Function
+
+function pickFirstNum(){
+
+$('.number').on('click', function(){
+if(isFirstNum) {
+
+firstNum = $(this).attr('value');
+firstNumResult += firstNum;
+    //testing
+    console.log('Fist num is ' + firstNum);
+    console.log('Fist num results is ' + firstNumResult);
+    }
+  });  
+}
+
+
+
+function pickOperator(){
+
+$('.operator').on('click', function(){
+  isFirstNum = false; //--- disables first num after opperator is used
+  op = $(this).attr('value');
+
+    if (op === 'plus'){
+        isAdd = true;
+        pickSecNum();
+    console.log('plus'); 
+
+    }if (op === 'minus'){
+        pickSecNum();
+    console.log('minus'); 
+    }
+  });  
+}
+
+function pickSecNum(){
+    $('.number').on('click', function() {
+
+    secondNum = $(this).attr('value');
+    secondNumResult += secondNum;
+
+    //testing
+    console.log('second num is ' + secondNum);
+    console.log('second num results is ' + secondNumResult);
+
+    if(isFirstNum === false) {
+    if(isAdd = true) {
+    total = parseInt(firstNumResult) + parseInt(secondNumResult); // converts from strings ie 1+2+3 = 123 rather than 6
+    console.log('total is ' + total);
+    }
+
+    if(isMinus = true) {
+    total = parseInt(firstNumResult) - parseInt(secondNumResult); // converts from strings ie 1+2+3 = 123 rather than 6
+    console.log('total is ' + total);
+    }
+    }
+});
+}
+
+
+function totalResult(){
+$('.total').on('click', function(){
+
+    console.log('total is ' + total);
+    totalResult();
+    });
+}
+
+
+
+
+
+//----
+pickFirstNum();
+pickOperator();
